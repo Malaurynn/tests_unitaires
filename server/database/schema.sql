364c352a95e22ddffcff656e845e7a2b70cdc7a9
Malaurynn <malauryn@icloud.com>
2025-01-14 10:58:06 +0100

    correction des erreurs bdd

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,20 +1,20 @@
 CREATE TABLE wilder (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
   lastname VARCHAR(255) NOT NULL,
-  firstname VARCHAR(255) NOT NULL,
+  firstname VARCHAR(255) NOT NULL
 );
 
 INSERT INTO wilder (lastname, firstname) 
 VALUES 
-('Alexandre', 'Clasquin')
-('Badreddine', 'Sahili')
-('Clément', 'Fiquet')
-('Fabien', 'Verhulst')
-('Giovanni', 'David')
-('Ludovic', 'Plougonven')
-('Ludovic', 'Pouillie')
-('Malaury', 'Nezan')
-('Mike', 'Hatchi')
-('Nicolas', 'Peria')
-('Philippe', 'Van Der Eecken')
-('Simon', 'Griscelli')
+('Alexandre', 'Clasquin'),
+('Badreddine', 'Sahili'),
+('Clément', 'Fiquet'),
+('Fabien', 'Verhulst'),
+('Giovanni', 'David'),
+('Ludovic', 'Plougonven'),
+('Ludovic', 'Pouillie'),
+('Malaury', 'Nezan'),
+('Mike', 'Hatchi'),
+('Nicolas', 'Peria'),
+('Philippe', 'Van Der Eecken'),
+('Simon', 'Griscelli');
